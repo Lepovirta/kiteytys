9788e9da71b3d81d9b936fbb3c448ba737de7778
Jaakko Pallari <jkpl@lepovirta.org>
2016-03-12 18:37:47 +0000

    Fixed size thread pool for PDF generation

--- src/main/scala/neljas/pdf/PDF.scala
+++ src/main/scala/neljas/pdf/PDF.scala
@@ -2,8 +2,13 @@ package neljas.pdf
 
 import io.github.cloudify.scala.spdf._
 import java.io._
+import java.util.concurrent.Executors
+import scalaz.concurrent.Task
 
 object PDF {
+
+  implicit val pdfThreadPool = Executors.newFixedThreadPool(2)
+
   val pdf = Pdf(new PdfConfig {
     userStyleSheet := "static/pdf.css"
     marginTop := "0mm"
@@ -12,10 +17,11 @@ object PDF {
     marginRight := "0mm"
   })
 
-  // TODO: error handlind?
-  def generate(page: String): Array[Byte] = {
+  def generate(page: String): Task[Array[Byte]] = Task.fork {
     val output = new ByteArrayOutputStream
-    val _ = pdf.run(page, output)
-    output.toByteArray
+    val result = pdf.run(page, output)
+
+    if (result == 0) Task.now(output.toByteArray)
+    else Task.fail(new RuntimeException(s"PDF generation failed. Result code: $result"))
   }
 }
