626027cab0a4e17a4530ee9016b68a58ff875ccd
Matias Keveri <keveri@users.noreply.github.com>
2016-03-12 20:10:31 +0200

    Add conf file containing defaults.

--- src/main/scala/neljas/Http.scala
+++ src/main/scala/neljas/Http.scala
@@ -1,5 +1,6 @@
 package neljas
 
+import com.typesafe.config._
 import org.http4s.headers.{`Content-Type`}
 import org.http4s.MediaType._
 import org.http4s.{UrlForm, HttpService}
@@ -12,13 +13,14 @@ import neljas.pdf.PDF
 
 object Http extends TwirlInstances {
 
+  private val conf   = ConfigFactory.load()
   private val static = cachedResource(Config("/static", "/static"))
 
   val route = HttpService {
     case r @ GET -> "static" /: _ => static(r)
 
-    case GET -> Root / "ping" =>
-      Ok("pong")
+    case GET -> Root / "conf" =>
+      Ok(conf.getString("smtp.port"))
 
     case GET -> Root =>
       Ok(html.index.render())
