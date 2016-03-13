bbbfdcaf84a16da063b0666fdfd5602b4bcdaca9
Jaakko Pallari <jkpl@lepovirta.org>
2016-03-13 14:52:33 +0000

    Database init, access, and HTTP shell

--- src/main/scala/neljas/Types.scala
+++ src/main/scala/neljas/Types.scala
@@ -1,29 +1,50 @@
 package neljas
 
 import org.http4s.UrlForm
-
+import argonaut._
+import Argonaut._
 import scala.util.Try
 import scalaz.Scalaz._
+import scalaz.ValidationNel
 
-object User {
-  def fromForm(form: UrlForm): Either[String, User] = {
-    val result = validateForm(form)(User.apply)
+
+object FormParsing {
+  def resultToEither[A](result: ValidationNel[String, A]): Either[String, A] =
     result.toEither.leftMap(_.list.mkString(". "))
-  }
 
-  private def validateForm(form: UrlForm) =
-    stringField(form, "name") |@| intField(form, "age")
-
-  private def stringField(form: UrlForm, field: String) =
+  def stringField(form: UrlForm, field: String): ValidationNel[String, String] =
     form.getFirst(field)
       .toSuccess(s"Missing field '$field'")
       .toValidationNel
 
-  private def intField(form: UrlForm, field: String) =
+  def intField(form: UrlForm, field: String): ValidationNel[String, Int] =
     form.getFirst(field)
       .flatMap(v => Try(v.toInt).toOption)
       .toSuccess(s"Invalid format for field '$field'")
       .toValidationNel
 }
 
+object User {
+  import FormParsing._
+
+  def fromForm(form: UrlForm): Either[String, User] = {
+    val result = (stringField(form, "name") |@| intField(form, "age"))(User.apply)
+    resultToEither(result)
+  }
+}
+
 final case class User(name: String, age: Int)
+
+object Message {
+  import FormParsing._
+
+  def fromForm(form: UrlForm): Either[String, Message] = {
+    val result = (stringField(form, "title") |@| stringField(form, "content"))(Message.apply)
+    resultToEither(result)
+  }
+
+  implicit val messageJsonCodec: CodecJson[Message] =
+    casecodec2(Message.apply, Message.unapply)("title", "content")
+}
+
+final case class Message(title: String, content: String)
